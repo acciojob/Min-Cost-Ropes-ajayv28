86e2a20821845c10f69e1590e93ef700c473cb48
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-09-15 11:27:44 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function mincost(arr)
 { 
 //write your code here
+	let cost = 0;
+	arrr.sort((a,b) => a-b);
+	
+	while(arr.length > 1){
+		let first = arr.shift();
+		let second = arr.shift();
+		cost += first+second;
+		arr.push(first + second);
+		arrr.sort((a,b) => a-b);
+	}
 // return the min cost
   
 }
