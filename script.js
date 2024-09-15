d8bcf3a7a62f3c0f6919e16531aae5d622accd06
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-09-15 11:42:55 +0300

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ function mincost(arr)
 { 
 //write your code here
 	let cost = 0;
-	arrr.sort((a,b) => a-b);
+	arr.sort((a,b) => a-b);
 	
 	while(arr.length > 1){
 		let first = arr.shift();
 		let second = arr.shift();
 		cost += first+second;
 		arr.push(first + second);
-		arrr.sort((a,b) => a-b);
+		arr.sort((a,b) => a-b);
 	}
 	return cost;
 // return the min cost
